bda2e9f763801a191bfd711f41946922c6889984
Srinivasa Reddy Somu <srinivasa.somu@e2open.com>
2020-02-25 21:33:35 +0530

    filesTable Completion

--- force-app/main/default/aura/FilesTable/FilesTableHelper.js
+++ force-app/main/default/aura/FilesTable/FilesTableHelper.js
@@ -1,16 +1,25 @@
 ({
     handleDoInit : function(cmp,event) {
+        debugger;
         var action = cmp.get("c.getData");
+        var recordId = cmp.get("v.recordId");
+        if(recordId) recordId = '0062v00001M3zvKAAR';
         var inputMap = {
             'recordId' : '0062v00001M3zvKAAR'
-        };//cmp.get("v.recordId")
+        };
         action.setParams({
             inputMap : inputMap
         });
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === 'SUCCESS') {
-                var res = response.getReturnValue();
+                var res = response.getReturnValue();                
+                var files = res.files;
+                for(var i=0;i<files.length;i++) {
+                    files[i]['Date'] = files[i].ContentDocument.ContentModifiedDate.toString().substring(0,10);
+                    files[i]['ContentSize'] = Number(files[i].ContentDocument.ContentSize)/1024;
+                }
+                console.log('res.files: '+JSON.stringify(res.files));
                 cmp.set("v.data",res.files);
             }
             else {
