c434c608a993f59dff05d6c107326830a4d6b305
Srinivasa Reddy Somu <srinivasa.somu@e2open.com>
2020-03-05 21:35:58 +0530

    Slot, Api

---
+++ force-app/main/default/lwc/contact/contact.js
@@ -0,0 +1,11 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Contact extends LightningElement {
+    //@api contactInfo;
+    /* = {
+        "Name" : "Sundar Pichai",
+        "FirstName" : "Sundar",
+        "LastName" : "Pichai",
+        "Email" : "sundar.pichai@google.com"
+    }*/
+}
--- force-app/main/default/lwc/contacts/contacts.js
+++ force-app/main/default/lwc/contacts/contacts.js
@@ -1,9 +1,24 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactsCtrl.getContacts';
 
 export default class Contacts extends LightningElement {
-    @wire(getContacts)
-    contactsLst;
+    @track data;
+    @track error;
+    searchKey = 'T';
+    /*@wire(getContacts)
+    contactsLst;*/
     //contactsLst.data --> method returned data
     //contactsLst.error --> errror information    
+    @wire(getContacts,{"fnFilter":"$searchKey"})
+    contactsLst({
+        data,
+        error
+    }) {
+        if(data) {
+            this.data = data;
+        }
+        else if(error) {
+            this.error = error;
+        }
+    }
 }
